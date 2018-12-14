5d6d9c0fab129ba6f8c76c21cf49882449f60ca1
h1801397 <43990446+h1801397@users.noreply.github.com>
2018-12-14 13:49:27 +0000

    Add files via upload

---
+++ EX16.py
@@ -0,0 +1,15 @@
+richter=float(input("Enter a magnitude on the Richter scale:"))
+if richter>=8.0:
+    print("Most structures fall")
+elif richter>=7.0:
+    print("Many buildings destroyed") 
+elif richter>=6.0:
+    print("Many buildings considerably dameged,same collapse")
+elif richter>=4.5:
+    print("Damage to poorly constructed buildings")
+else:
+    print("No destruction of buildings")
+
+
+    
+    
